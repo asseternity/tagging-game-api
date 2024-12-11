2b7b509c407f858bdbd802d9e718fa8c5ce4512a
asseternity <nakupovasset@gmail.com>
2024-12-11 22:56:09 +0400

    feat: init api

---
+++ app.js
@@ -0,0 +1,17 @@
+// dependencies
+require("dotenv").config();
+const express = require("express");
+const path = require("node:path");
+const app = express();
+
+// settings
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// launch
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}!`);
+});
